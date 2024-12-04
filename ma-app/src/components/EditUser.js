8e7add389852f8febd3a2cdd86166f483c79d099
Shivesh Nandan <kshivesh929@gmail.com>
2024-12-04 21:43:33 +0530

    completed

--- ma-app/src/components/EditUser.js
+++ ma-app/src/components/EditUser.js
@@ -43,13 +43,6 @@ const EditUsers = () => {
     UsersData();
   }, [CurrentPage])
 
-  const page = () => {
-    if (CurrentPage === 1) {
-      setCurrentPage(2);
-    } else {
-      setCurrentPage(1);
-    }
-  }
 
   const ToggleEditModal = (index) => {
     setCurrentId(index);
@@ -100,24 +93,24 @@ const EditUsers = () => {
         :
         <>
           <div className='flex justify-end p-4 bg-slate-600'>
-            <p onClick={() => { navigation("/profile") }} className='bg-green-500 w-fit px-2 py-1 rounded-lg cursor-pointer'>User Profile</p>
+            <p onClick={() => { navigation("/Users") }} className='bg-green-500 w-fit px-2 py-1 rounded-lg cursor-pointer'>Users List</p>
           </div>
-          <div className='flex justify-center pt-12 transition-all'>
-            <h1 onClick={() => { setTable(true) }} className={`${Table ? "bg-blue-700" : ""} text-xl px-2 py-1 cursor-pointer rounded-l-lg transition-all border`}>Table Format</h1>
-            <h1 onClick={() => { setTable(false) }} className={`${!Table ? "bg-blue-700" : ""} text-xl px-2 py-1 cursor-pointer rounded-r-lg transition-all border`}>Card Format</h1>
+          <div className='flex justify-center pt-12 mb-3 transition-all'>
+            <h1 onClick={() => { setTable(true) }} className={`${Table ? "bg-blue-700" : ""} text-xl px-5 font-semibold py-1 cursor-pointer rounded-l-lg transition-all border`}>Table Format</h1>
+            <h1 onClick={() => { setTable(false) }} className={`${!Table ? "bg-blue-700" : ""} text-xl px-5 font-semibold py-1 cursor-pointer rounded-r-lg transition-all border`}>Card Format</h1>
           </div>
-          <div className='flex flex-col items-center justify-end max-sm:text-xs max-sm:overflow-x-scroll mx-2 pb-3'>
+          <div className='flex flex-col sm:items-center justify-end max-sm:text-xs max-sm:overflow-x-scroll mx-2 pb-3'>
             {Table ?
               <>
                 <div className='mt-5'>
                   <thead className='bg-slate-800'>
                     <tr>
-                      <th className='border-sky-100 border px-2'>S.no</th>
-                      <th className='border-sky-100 border px-2'>First Name</th>
-                      <th className='border-sky-100 border px-2'>Last name</th>
-                      <th className='border-sky-100 border px-2'>Email</th>
-                      <th className='border-sky-100 border px-2'>Avatar</th>
-                      <th className='border-sky-100 border px-2'>Edit</th>
+                      <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>S.no</th>
+                      <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>First Name</th>
+                      <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Last name</th>
+                      <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Email</th>
+                      <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Avatar</th>
+                      <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Edit</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -144,13 +137,17 @@ const EditUsers = () => {
               <>
                 <div className='flex flex-wrap justify-center w-[700px] max-md:w-full'>
                   {UserList.map((user, index) => (
-                    <div className='flex flex-col w-5/12 bg-slate-800 rounded-lg justify-center items-center p-2 m-3'>
+                    <div className='flex flex-col w-[43%] bg-slate-800 rounded-lg justify-center items-center p-2 m-3'>
                       <img src={`${user.avatar}`} alt="" className='bg-white rounded-full w-20 h-20 mb-2' />
                       <p className=''>Name :</p>
                       <p className='font-bold mb-2'>{user.first_name} {user.last_name}</p>
                       {/* <p className='font-bold'>{user.last_name}</p> */}
                       <p className=''>email : </p>
-                      <p className=''>{user.email}</p>
+                      <p className=' p-1 text-xs '>{user.email}</p>
+                  <div className='text-center flex '>
+                  <p onClick={() => { ToggleEditModal(index) }} className='bg-green-500 px-2 rounded m-2 cursor-pointer'>Edit</p>
+                  <p onClick={() => { ToggleDeleteModal(index) }} className='bg-red-500 px-2 rounded m-2 cursor-pointer'>Delete</p>
+                  </div>
                     </div>
                   ))}
                 </div>
@@ -159,7 +156,7 @@ const EditUsers = () => {
           </div>
         </>
       }
-      <p onClick={() => { page() }} className='text-center text-sm mb-5 cursor-pointer'>page {CurrentPage} of {TotalPage}</p>
+      <p className='text-sm mb-5 cursor-pointer flex items-center justify-center'><span onClick={() => {setCurrentPage(1)}} className=' px-1 mx-2 text-center font-bold text-xl '>{"<"}</span>page {CurrentPage} of {TotalPage}<span onClick={() => {setCurrentPage(2)}} className=' px-1 mx-2 text-center font-bold text-xl '>{">"}</span></p>
 
       <div className={`${!modal ? "hidden" : ""} bg-[#000000a6] w-full h-screen fixed top-0 right-0`}>
         <div className='flex h-[80vh] '>
--- ma-app/src/components/Users.js
+++ ma-app/src/components/Users.js
@@ -37,13 +37,6 @@ const Users = () => {
       UsersData();
     }, [CurrentPage])
     
-    const page = () => {
-        if(CurrentPage === 1){
-            setCurrentPage(2);
-        }else{
-            setCurrentPage(1);
-        }
-    }
 
   return (
     <div className='text-white transition-all'>
@@ -57,23 +50,23 @@ const Users = () => {
       :
       <>
       <div className='flex justify-end p-4 bg-slate-600'>
-        <p onClick={() => {navigation("/profile")}} className='bg-green-500 w-fit px-2 py-1 rounded-lg cursor-pointer'>User Profile</p>
+        <p onClick={() => {navigation("/profile")}} className='bg-green-500 w-fit px-2 py-1 rounded-lg cursor-pointer'>Edit User</p>
       </div>
-            <div className='flex justify-center pt-12 transition-all'>
-         <h1 onClick={() => {setTable(true)}}className={`${Table ? "bg-blue-700" : ""} text-xl px-2 py-1 cursor-pointer rounded-l-lg transition-all border`}>Table Format</h1>
-         <h1 onClick={() => {setTable(false)}} className={`${!Table ? "bg-blue-700" : ""} text-xl px-2 py-1 cursor-pointer rounded-r-lg transition-all border`}>Card Format</h1>
+            <div className='flex justify-center pt-12 mb-3 transition-all'>
+         <h1 onClick={() => {setTable(true)}}className={`${Table ? "bg-blue-700" : ""} text-xl px-5 font-semibold py-1 cursor-pointer rounded-l-lg transition-all border`}>Table Format</h1>
+         <h1 onClick={() => {setTable(false)}} className={`${!Table ? "bg-blue-700" : ""} text-xl px-5 font-semibold py-1 cursor-pointer rounded-r-lg transition-all border`}>Card Format</h1>
             </div>
-        <div className='flex flex-col items-center justify-end max-sm:text-xs max-sm:overflow-x-scroll mx-2 pb-3'>
+        <div className='flex flex-col sm:items-center justify-end max-sm:text-xs max-sm:overflow-x-scroll mx-2 pb-3'>
        {Table ? 
        <>
         <div className='mt-5'>   
-        <thead className='bg-slate-800'>
+        <thead className='bg-slate-800 '>
             <tr>
-                <th className='border-sky-100 border px-2'>S.no</th>
-                <th className='border-sky-100 border px-2'>First Name</th>
-                <th className='border-sky-100 border px-2'>Last name</th>
-                <th className='border-sky-100 border px-2'>Email</th>
-                <th className='border-sky-100 border px-2'>Avatar</th>
+                <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>S.no</th>
+                <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>First Name</th>
+                <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Last name</th>
+                <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Email</th>
+                <th className='border-sky-100 border px-2 py-2 min-w-[95px]'>Avatar</th>
             </tr>
         </thead>
         <tbody>
@@ -96,13 +89,13 @@ const Users = () => {
        <>
         <div className='flex flex-wrap justify-center w-[700px] max-md:w-full'>
         {UserList.map((user,index) => (
-        <div className='flex flex-col w-5/12 bg-slate-800 rounded-lg justify-center items-center p-2 m-3'>
+        <div className='flex flex-col w-[43%] bg-slate-800 rounded-lg justify-center items-center p-2 m-3'>
             <img src={`${user.avatar}`} alt="" className='bg-white rounded-full w-20 h-20 mb-2'/> 
             <p className=''>Name :</p>
             <p className='font-bold mb-2'>{user.first_name} {user.last_name}</p>
             {/* <p className='font-bold'>{user.last_name}</p> */}
             <p className=''>email : </p>
-            <p className=''>{user.email}</p>
+            <p className='mb-3'>{user.email}</p>
         </div>
         ))}
         </div>
@@ -111,7 +104,7 @@ const Users = () => {
     </div>
       </>
       }
-      <p onClick={() => {page()}} className='text-center text-sm mb-5 cursor-pointer'>page {CurrentPage} of {TotalPage}</p>
+      <p className='text-sm mb-5 cursor-pointer flex items-center justify-center'><span onClick={() => {setCurrentPage(1)}} className=' px-1 mx-2 text-center font-bold text-xl '>{"<"}</span>page {CurrentPage} of {TotalPage}<span onClick={() => {setCurrentPage(2)}} className=' px-1 mx-2 text-center font-bold text-xl '>{">"}</span></p>
     </div>
   )
 }
